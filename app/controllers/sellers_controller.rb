a6dc51c712340d7cb1caf4c0c863a5bd40cf1668
Frederico Murakawa <fredbmurakawa@gmail.com>
2017-11-20 18:21:52 -0200

    seller controller and views

---
+++ app/controllers/sellers_controller.rb
@@ -0,0 +1,25 @@
+class SellersController < ApplicationController
+
+  def new
+    @seller = Seller.new
+  end
+
+  def create
+    @seller = Seller.new(seller_params)
+
+    if @seller.save
+      current_user.seller = @seller
+      current_user.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def seller_params
+    params.require(:seller).permit(:cnpj, :name, :address, :address_number, :address_complement, :neighborhood, :city, :state, :zip_code)
+  end
+
+end
