8d576034a1c217122f1a2871e1e2092feaa784be
Frederico Murakawa <fredbmurakawa@gmail.com>
2017-11-20 18:31:13 -0200

    sellers improve

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -8,7 +8,7 @@ class SellersController < ApplicationController
     @seller = Seller.new(seller_params)
 
     if @seller.save
-      current_user.seller = @seller
+      current_user.seller_id = @seller
       current_user.save
       redirect_to user_path(current_user)
     else
@@ -19,7 +19,7 @@ class SellersController < ApplicationController
   private
 
   def seller_params
-    params.require(:seller).permit(:cnpj, :name, :address, :address_number, :address_complement, :neighborhood, :city, :state, :zip_code)
+    params.require(:seller).permit(:cnpj, :name, :address, :address_number, :address_complement, :neighborhood, :city, :state, :zip_code, :phone_number, :number_of_employees)
   end
 
 end
