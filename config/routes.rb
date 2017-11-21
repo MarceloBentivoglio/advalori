280f04b3262b218efc5142c0a36c74b567beaadc
Frederico Murakawa <fredbmurakawa@gmail.com>
2017-11-21 18:33:05 -0200

    investors crud

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :sellers
+  resources :investors
   resources :invoice_payers, only: [:new, :create, :destroy, :show]
 
   resources :invoices, only: [:new, :create, :index]
