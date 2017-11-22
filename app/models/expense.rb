3edf941590305063c4dc49844e4e1a9f28aa2d28
Frederico Murakawa <fredbmurakawa@gmail.com>
2017-11-22 16:51:34 -0200

    validations models v2

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,5 @@
 class Expense < ApplicationRecord
   belongs_to :seller
+
+  validates :supplier, :value, :due_date, :seller_id, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :invoice
   belongs_to :investor
+
+  validates :investor_id, :invoice_id, presence: true
 end
