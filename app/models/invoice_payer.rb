989e6e04f37f8f762e474ca6765abf2fef9d8727
Frederico Murakawa <fredbmurakawa@gmail.com>
2017-11-22 16:20:04 -0200

    validations models

--- app/models/invoice_payer.rb
+++ app/models/invoice_payer.rb
@@ -1,3 +1,5 @@
 class InvoicePayer < ApplicationRecord
   has_many :invoices
+
+  validates :cnpj, :name, :address, :address_number, :city, :neighborhood, :zip_code, :state, :phone_number, presence: true
 end
