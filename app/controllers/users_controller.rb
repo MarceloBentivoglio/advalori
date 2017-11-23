f55e4bc045b094323a6b1d38f4c6c18aa234d93f
Cassiano Mangini <cassiano.mangini@gmail.com>
2017-11-23 11:11:23 -0200

    alert created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,15 @@ class UsersController < ApplicationController
   def show
     @invoice = Invoice.new
     @user = current_user
+    welcome_message unless @user.seller_id
+  end
+
+  private
+
+  def welcome_message
+    flash[:notice] = "Please complete your profile <a href=#{new_user_seller_path}>
+    here</a>".html_safe
   end
 end
+
+
