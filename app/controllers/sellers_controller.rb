8589a6f8528fedf551ae15f5d0ba250cf5beb7d5
Cassiano Mangini <cassiano.mangini@gmail.com>
2017-11-23 17:29:58 -0200

    fixed seller controller

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -9,6 +9,18 @@ class SellersController < ApplicationController
     @seller = Seller.new
   end
 
+  def show
+    @seller = Seller.find(current_user)
+  end
+
+  def update
+    @seller = Seller.find(current_user)
+    @seller.update(seller_params)
+
+    redirect_to user_path
+    sucess_message
+  end
+
   def create
     @seller = Seller.new(seller_params)
 
@@ -25,6 +37,10 @@ class SellersController < ApplicationController
 
   def seller_params
     params.require(:seller).permit(:cnpj, :name, :address, :address_number, :address_complement, :neighborhood, :city, :state, :zip_code, :phone_number, :number_of_employees)
+  end
+
+   def sucess_message
+    flash[:notice] = "Your profile has been successfully updated"
   end
 
 end
