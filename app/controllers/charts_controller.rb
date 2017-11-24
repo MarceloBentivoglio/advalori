229152a711501a7a27a3efc72f620a410a83476c
Frederico Murakawa <fredbmurakawa@gmail.com>
2017-11-24 16:45:45 -0200

    dashboard view v0

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -8,8 +8,8 @@ class ChartsController < ApplicationController
 
   def calc_expenses_chart
     today_date = Time.now
-    expenses = Expense.where(seller_id: current_user).where('due_date > ?', DateTime.now).group_by_month(:due_date).sum("-value")
-    installments = Installment.joins(:invoice).where("invoices.seller_id" => current_user).where('due_date > ?', DateTime.now).group_by_month(:due_date).sum("value")
+    expenses = Expense.where(seller_id: current_user.seller).where('due_date > ?', DateTime.now).group_by_day(:due_date).sum("-value")
+    installments = Installment.joins(:invoice).where("invoices.seller_id" => current_user.seller).where('due_date > ?', DateTime.now).group_by_day(:due_date).sum("value")
     cash_flow = expenses.merge(installments) {|date, expense, installment| expense + installment }
     render json: cash_flow
   end
