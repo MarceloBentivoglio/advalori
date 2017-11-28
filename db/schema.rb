3a82e5aafeb16d1de3946e9e9858f7e9085e2163
Frederico Murakawa <fredbmurakawa@gmail.com>
2017-11-28 15:09:12 -0200

    order controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127184749) do
+ActiveRecord::Schema.define(version: 20171128170742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 20171127184749) do
     t.integer  "investor_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "fee"
     t.index ["investor_id"], name: "index_orders_on_investor_id", using: :btree
     t.index ["invoice_id"], name: "index_orders_on_invoice_id", using: :btree
   end
